a5af2ce581d9d808bacfbe0bbaf9612274e732a8
YashBarbole <yashbarbole7110@gmail.com>
2025-07-05 17:15:25 +0530

    2pointer

---
+++ TRAPWATER.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>  
+using namespace std;
+
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n=height.size();
+        int leftmax=0;
+         int rightmax=0;
+          int total=0;
+        int l=0;
+        int r=n-1;
+
+        while(l<r){
+            if(height[l]<=height[r]){
+                  if(leftmax>height[l]){
+                total+=leftmax-height[l];
+                 }
+                    else{
+                leftmax=height[l];
+               
+            }
+             l++;
+            }
+          
+         
+            else{
+                if(rightmax>=height[r]){
+                    total+=rightmax-height[r];
+                }
+                else{
+                    rightmax=height[r];
+                   
+                }
+                 r--;
+            }
+
+
+        }
+        return total;
+        
+    }
+};
+
+int main(){
+    vector<int> height={1,0,2,0,4,2};
+    Solution sol;
+    int res=sol.trap(height);
+
+    cout<<"the total water trapped is"<<res<<endl;
+    return 0;
+    
+
+    
+}
