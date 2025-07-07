eebb153d5ac1caaff7c8895abd2c818b36579aa9
YashBarbole <yashbarbole7110@gmail.com>
2025-07-08 00:52:22 +0530

    easy brooo

---
+++ arraytolinkedL.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    public:
+    int data;
+    Node*next;
+
+    Node(int data1, Node* next1){
+        data=data1;
+        next=next1;
+    }
+
+    Node(int data1){
+        data=data1;
+        next=nullptr;
+    }
+
+
+};
+
+Node* convertArr2LL(vector<int> arr){
+    Node* head= new Node(arr[0]);
+    Node* mover= head;
+
+    for(int i=0;i<arr.size();i++){
+        Node*temp = new Node(arr[i]);
+        mover->next=temp;
+        mover=temp;
+
+    }
+        return head;
+} 
+//yash
+int main(){
+    vector<int> arr={2,5,6,7};
+
+    Node*head =convertArr2LL(arr);
+    cout<< head->data;
+}
