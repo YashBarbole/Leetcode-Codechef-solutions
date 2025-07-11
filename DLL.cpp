e2e2c3b141b6710587756e2802c984bf6e3048a4
YashBarbole <yashbarbole7110@gmail.com>
2025-07-11 22:36:27 +0530

    all functions added

--- DLL.cpp
+++ DLL.cpp
@@ -54,10 +54,63 @@ void print(Node*head){
 
 }
 
+Node* deleteTail(Node*head){
+       if(head==NULL || head->next==NULL){
+        return NULL;   
+    }
+    Node*tail=head;
+    while(tail->next!=NULL){
+       tail= tail->next;
+    }
+    Node*newTail=tail->back;
+    newTail->next=nullptr;
+    tail->back=nullptr;
+    delete tail;
+    return head;
+
+}
+
+Node* deleteK(Node*head,int k){
+    if (head==NULL){
+        return NULL;
+    }
+    int cnt=0;
+    Node*knode=head;
+    while(knode!=NULL){
+        cnt++;
+        if(cnt==k) break;
+        knode=knode->next;
+    }
+    Node*prev=knode->back;
+    Node*front=knode->next;
+
+    if(prev==NULL && front == NULL) return NULL;
+
+    else if(prev==NULL){
+      return   deleteHead(head);
+    }
+
+    else if(front==NULL){
+       return  deleteTail(head);
+    }
+
+    else{
+        prev->next=front;
+        front->back=prev;
+
+        knode->next=nullptr;
+        knode->back=nullptr;
+        delete knode;
+    }
+    return head;
+
+
+}
+
 int main(){
     vector <int> arr={12,5,8,7};
     Node*head=convertArr2Dll(arr);
-    head=deleteHead(head);
+    head=deleteK(head,1);
     print(head);
 
 
