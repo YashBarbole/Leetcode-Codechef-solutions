15ac9373d2b538e943edd3357f24b95dfff96cb1
YashBarbole <yashbarbole7110@gmail.com>
2025-07-23 08:28:53 +0530

    java

---
+++ inputs.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class inputs {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter age");
+        int age=sc.nextInt();
+        System.out.println("your age is"+age);
+        
+        
+    }
+    
+}
---
+++ yash.java
@@ -0,0 +1,11 @@
+public class yash{
+
+    public static void main(String[] args){
+        int age=26;
+        int marks=99;
+
+    }
+
+
+    
+}
