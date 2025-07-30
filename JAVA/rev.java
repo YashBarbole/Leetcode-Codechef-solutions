92d0f02a2103381134f5c8b771e663c6275fe715
YashBarbole <yashbarbole7110@gmail.com>
2025-07-30 11:14:05 +0530

    swap and do

---
+++ JAVA/rev.java
@@ -0,0 +1,33 @@
+package JAVA;
+
+import java.util.Arrays;
+
+public class rev {
+    public static void main(String[] args) {
+          int [] arr={1,3,4,5};
+        System.out.println(Arrays.toString(arr));
+        rev(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+
+     static void rev(int []arr){
+        int s=0;
+        int e=arr.length-1;
+       while(s<e){
+        swap(arr, s, e);
+        s++;
+        e--;
+       }
+    }
+
+
+     static void swap(int [] arr,int index1,int index2){
+        int temp= arr[index1];
+        arr[index1]=arr[index2];
+        arr[index2]=temp;
+
+    }
+        
+    
+}
