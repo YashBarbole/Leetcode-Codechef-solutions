92cd241f8810679b0f44b839df007d4b75c3ae73
YashBarbole <yashbarbole7110@gmail.com>
2025-09-07 21:14:53 +0530

    easy

---
+++ REVISE_STRINGS/BasicStr.java
@@ -0,0 +1,13 @@
+package REVISE_STRINGS;
+
+public class BasicStr {
+    public static void main(String[] args) {
+        String a="yash";
+        String b="yash";
+
+        System.out.println(a);
+        a="king";
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
